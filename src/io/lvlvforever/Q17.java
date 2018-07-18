40463a4919b4821f6ceb7ffcac72f5ae3224042f
lvlv <lvlvbuaa@163.com>
2018-07-18 23:05:01 +0800

    面试题第14 15 16 17题

---
+++ src/io/lvlvforever/Q17.java
@@ -0,0 +1,20 @@
+package io.lvlvforever;
+
+/**
+ * Created by lvlvforever on 2018/7/18.
+ */
+public class Q17 {
+    public ListNode Merge(ListNode list1, ListNode list2) {
+        if (list1 == null) {
+            return list2;
+        } else if (list2 == null) {
+            return list1;
+        }
+        ListNode head = new ListNode(0);
+        while (list1 != null && list2 != null) {
+
+        }
+
+
+    }
+}
