97675414fc4287fb75a3584765c55b9a1ec850d9
lvlv <lvlvbuaa@163.com>
2018-07-19 17:54:37 +0800

    面试题第16 17 18 题

--- src/io/lvlvforever/Q17_2.java
+++ src/io/lvlvforever/Q17_2.java
@@ -1,20 +1,18 @@
 package io.lvlvforever;
 
-/**
- * Created by lvlvforever on 2018/7/18.
- */
-public class Q17 {
+public class Q17_2 {
     public ListNode Merge(ListNode list1, ListNode list2) {
         if (list1 == null) {
             return list2;
         } else if (list2 == null) {
             return list1;
         }
-        ListNode head = new ListNode(0);
-        while (list1 != null && list2 != null) {
-
+        if (list1.val < list2.val) {
+            list1.next = Merge(list1.next, list2);
+            return list1;
+        } else {
+            list2.next = Merge(list1, list2.next);
+            return list2;
         }
-
-
     }
 }
