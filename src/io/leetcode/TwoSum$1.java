309fe76382b660f8b672504aab20270c389fcab0
daipeng <daipeng@yuewen.com>
2018-12-20 18:50:47 +0800

    re

---
+++ src/io/leetcode/TwoSum$1.java
@@ -0,0 +1,16 @@
+/**
+ * Description:
+ * User: daipeng
+ * Date: 2018-12-20 9:54
+ * Project:SolutionForCodingInterview
+ */
+package io.leetcode;
+
+public class TwoSum$1 {
+    public static void main(String[] args) {
+
+    }
+    public int[] twoSum(int[] nums, int target) {
+
+    }
+}
--- src/io/lvlvforever/Main.java
+++ src/io/lvlvforever/Main.java
@@ -3,8 +3,13 @@ package io.lvlvforever;
 public class Main {
     public static void main(String[] args) {
 
+
         int[][] matrix = {{1, 2, 3}, {1, 1}};
         System.err.println(matrix.length);
         System.err.println(matrix[0].length);
+
+        System.err.println("hello world");
+
+
     }
 }
