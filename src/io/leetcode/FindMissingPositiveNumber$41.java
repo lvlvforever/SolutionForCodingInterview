537e9cc208bcbd4f32f9f0a961eb95a010b4bb80
lvlv <lvlvbuaa@163.com>
2019-01-04 23:13:36 +0800

    41

---
+++ src/io/leetcode/FindMissingPositiveNumber$41.java
@@ -0,0 +1,18 @@
+package io.leetcode;
+
+import java.util.BitSet;
+
+/**
+ * @author lvlvforever
+ * @createTime 2018-12-30 15:17
+ * @description
+ */
+public class FindMissingPositiveNumber$41 {
+    public static void main(String[] args) {
+        int[] nums = {};
+        FindMissingPositiveNumber$41 obj = new FindMissingPositiveNumber$41();
+        System.err.println(obj.firstMissingPositive(nums));
+
+    }
+
+}
